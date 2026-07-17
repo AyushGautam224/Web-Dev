16885b7d409768bf6a5769fe46a7ac0a4a7e7b4e
Ayush Gautam <155223728+AyushGautam224@users.noreply.github.com>
2026-07-17 20:44:29 +0530

    Add files via upload

---
+++ Day 54  JS/script.js
@@ -0,0 +1,16 @@
+alert("Go back Nonest")
+
+console.log("code is running")
+console.log("code is also running")
+console.log("code is running and burning")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure want to leave this page and blast the PC")
+
+if(isTrue){
+    console.log("computer is blasting")
+}
+else {
+    console.log("computer is not blasting")
+}
+console.log("your number is " +a)
