1ed87a2deaac06ec8eb72cddad21b11900a07e22
Ayush Gautam <155223728+AyushGautam224@users.noreply.github.com>
2026-07-18 21:19:00 +0530

    Add files via upload

---
+++ Day 55 JS/script.js
@@ -0,0 +1,42 @@
+console.log("HEy its tuuorial 55")
+
+//var a = 5;
+//var b = 6;
+//var c = "ayush"
+//var _a = 55;
+//var 55a = 45; not allowed
+
+let a = 5;
+//a = a + 1
+let b = 6;
+let c = "ayush";
+let _a = "shubham";
+
+console.log(a + b + 8)
+console.log(typeof a, typeof b, typeof c)
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+//const a1 =  6;
+// a1 = a1 + 1; // not allowed because a1 is constant
+
+let x = "ayush gautam"
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name ": "harry",
+    "job code ":5600 ,
+    "is_handsome": true
+}
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
