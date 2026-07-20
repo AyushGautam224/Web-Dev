94a9389a3cb3080190858ad708f4421df4b761b6
Ayush Gautam <155223728+AyushGautam224@users.noreply.github.com>
2026-07-20 21:47:10 +0530

    Add files via upload

---
+++ Day 57 JS Loops/index.js
@@ -0,0 +1,41 @@
+console.log("i am a loop")
+
+// let a = 1;
+
+// // console.log(a)
+// // console.log(a+1)
+// // console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+// let obj = {
+//     name: ": ayush",
+//     role: ": programmer",
+//     company: " : ayushstate"
+// }
+
+// // for (const key in obj) {
+// //     if (Object.prototype.hasOwnProperty.call(obj, key)) {
+// //         const element = obj[key];
+// // console.log( key ,element)
+// //     }
+
+// for (const key in obj) {
+// console.log( key)
+// }
+
+// for (const c of "ayush") {
+//     console.log(c)
+// }
+
+// let i = 3;
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+let i = 10;
+do {
+    console.log(i)
+     i++;
+} while (i<6);
