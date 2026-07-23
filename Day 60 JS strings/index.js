16cad2479acc51d080b5bf1fb33228286bf89484
Ayush Gautam <155223728+AyushGautam224@users.noreply.github.com>
2026-07-23 21:24:17 +0530

    Add files via upload

---
+++ Day 60 JS strings/index.js
@@ -0,0 +1,24 @@
+let  a = "ayush";
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+// console.log(a[5])
+
+console.log(a.length)
+
+let name = " ayush"
+let friend = " aryan"
+
+console.log("His name is " + name + " and his friend name is " +friend)
+
+let b = " Anshu "
+console.log(b.toLocaleUpperCase())
+console.log(b.toLocaleLowerCase())
+console.log(b.length)
+console.log(b.slice(0 ,4))
+console.log(b.slice(3))
+console.log(b.replace("An", "Sudhan"))
+console.log(b.concat(a, " Neha", " Rakram"))
