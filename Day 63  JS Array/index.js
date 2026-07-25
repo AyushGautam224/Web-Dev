32ca08bf480a66670a32747df9d73b404ce556e3
Ayush Gautam <155223728+AyushGautam224@users.noreply.github.com>
2026-07-25 22:14:05 +0530

    Add files via upload

---
+++ Day 63  JS Array/index.js
@@ -0,0 +1,16 @@
+let arr = [1 ,2 , 3, 4, 5]
+
+arr[0] = 548;
+// pop to remove , push to insert ,splice to delete/add elements b/w the given range etc
+console.log(arr)
+console.log(arr.length)
+
+console.log(arr.toString)
+console.log(arr.join( " and "))
+
+let a1 = [1, 2, 3]
+let a2 = [4, 5, 6]
+let a3 = [7, 8, 9]
+
+b = a1.concat(a2 , a3)
+console.log(b)
