96346431b277682abf94092c4843795cd026ce37
Ayush Gautam <155223728+AyushGautam224@users.noreply.github.com>
2026-08-27 10:30:49 +0530

    Add files via upload

---
+++ Day 97 Exercise Dummy Data Generate/main.js
@@ -0,0 +1,17 @@
+// Generate a Dummy Data  in this formate in a collection called employee in a DB called company 
+
+// {
+//    name: "ayush",
+//    salary: 435$,
+//    language : "python ",
+//    Address: "office ",
+//    city : "london ",
+//    isManager: true
+
+// }
+
+// generate 10 such records when a button genrate data is clicked 
+
+// Create an express app with mongoose to achieve it 
+
+// Everytime button is clicked u should clear the  collection 
