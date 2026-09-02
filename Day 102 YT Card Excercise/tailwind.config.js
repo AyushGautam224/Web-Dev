009ffc02f23b018cefb1bc4518c437d4e2228da5
Ayush Gautam <155223728+AyushGautam224@users.noreply.github.com>
2026-09-02 21:02:28 +0530

    Add files via upload

---
+++ Day 102 YT Card Excercise/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}" // This tells it to look for all .html and .js files in the main folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
